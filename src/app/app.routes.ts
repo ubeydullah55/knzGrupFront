20643390925f3ac54b010aa6b5db52b58db756c8
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2024-07-11 00:30:24 +0300

    add products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomapageComponent } from './pages/front/homapage/homapage.component';
+import { ProductsComponent } from './pages/front/products/products.component';
 
 export const routes: Routes = [
     {
         path:'',
         component:HomapageComponent
+    },
+    {
+        path:'products',
+        component:ProductsComponent
     }
 ];
