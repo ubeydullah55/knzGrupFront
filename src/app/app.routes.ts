34a0ab1bedb90496867532e7ed9366bd57b3a272
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2024-07-11 01:54:16 +0300

    add contact and about pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomapageComponent } from './pages/front/homapage/homapage.component';
 import { ProductsComponent } from './pages/front/products/products.component';
+import { AboutComponent } from './pages/front/about/about.component';
+import { ContactComponent } from './pages/front/contact/contact.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,13 @@ export const routes: Routes = [
     {
         path:'products',
         component:ProductsComponent
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'contact',
+        component:ContactComponent
     }
 ];
