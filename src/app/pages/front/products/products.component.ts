0f437ac786c863db1a520ad8e59a732101b64ac7
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2024-08-17 18:54:25 +0300

    ekelme

--- src/app/pages/front/products/products.component.ts
+++ src/app/pages/front/products/products.component.ts
@@ -1,38 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
-import { productsModel } from '../../../models/productModel';
 import { ProductsService } from '../../../services/products.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { DataService } from '../../../services/data-service.service';
 import { CategoryService } from '../../../services/category.service';
 
-
-import { categoryModel } from '../../../models/categoryModel';
-
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css']  // Düzeltilmiş: 'styleUrls' yerine 'styleUrl' yerine
 })
 export class ProductsComponent {
 
-  constructor(private router: Router, private dataService: DataService) {}
-  cartService=inject(CartService);
-  productsService=inject(ProductsService);
-  categoryService=inject(CategoryService);
+  // Servisleri injection yoluyla al
+  cartService = inject(CartService);
+  productsService = inject(ProductsService);
+  categoryService = inject(CategoryService);
+  dataService = inject(DataService);
+
+  // TrackBy function for *ngFor
   trackById(index: number, item: any): number {
     return item.id; // Her öğe için benzersiz olan ID'yi döndür
   }
 
-  urunEkle(item:any){
+  // Sepete ürün ekleme
+  urunEkle(item: any) {
     this.cartService.urunEkle(item);  
   }
 
-  urunSil(id:number){
+  // Sepetten ürün silme
+  urunSil(id: number) {
     this.cartService.urunSil(id);
   }
-
 }
