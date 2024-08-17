00dedf93769a5db173bd6f1e500e83bc6f4aeba4
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2024-08-18 02:49:44 +0300

    sayfa yenileme işlemi çözüldü

--- src/app/pages/front/products/products.component.ts
+++ src/app/pages/front/products/products.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject,OnInit } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
 import { ProductsService } from '../../../services/products.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { DataService } from '../../../services/data-service.service';
 import { CategoryService } from '../../../services/category.service';
+
 
 @Component({
   selector: 'app-products',
@@ -13,14 +14,28 @@ import { CategoryService } from '../../../services/category.service';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']  // Düzeltilmiş: 'styleUrls' yerine 'styleUrl' yerine
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  say:number=0;
   // Servisleri injection yoluyla al
   cartService = inject(CartService);
   productsService = inject(ProductsService);
   categoryService = inject(CategoryService);
   dataService = inject(DataService);
-
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    // Sayfanın daha önce yenilenip yenilenmediğini kontrol et
+    const hasReloaded = localStorage.getItem('hasReloaded');
+  
+    if (!hasReloaded) {
+      // Yenileme işlemini gerçekleştir
+      localStorage.setItem('hasReloaded', 'true');
+      window.location.reload();
+    } else {
+      // Yenilenmişse, durumu sıfırla
+      localStorage.removeItem('hasReloaded');
+    }
+  }
   // TrackBy function for *ngFor
   trackById(index: number, item: any): number {
     return item.id; // Her öğe için benzersiz olan ID'yi döndür
