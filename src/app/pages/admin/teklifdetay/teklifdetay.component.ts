9712e1582dd08128c27f11a273d5feb1fa88db7f
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2024-08-19 20:13:09 +0300

    panel müsteri detayı eklendi

--- src/app/pages/admin/teklifdetay/teklifdetay.component.ts
+++ src/app/pages/admin/teklifdetay/teklifdetay.component.ts
@@ -14,39 +14,27 @@ import { CommonModule } from '@angular/common';
 export class TeklifdetayComponent {
   urlid:number;
   item: any;
+  musteriBilgi: any;
+  
   urunList: productsModel[] = [];
-  public teklifDetay :teklifDetayModel[]=[
-    { teklifid:1,productid: 1, productprice: 12,productimg:'product-01.jpg',productname:'ürün1',count:1},
-    { teklifid:1,productid: 2, productprice: 55,productimg:'product-01.jpg',productname:'ürün2',count:4},
-    { teklifid:2,productid: 3, productprice: 33,productimg:'product-01.jpg',productname:'ürün5',count:8},
-  ];
+
   constructor(private route: ActivatedRoute,private teklifService: TeklifdetayService) {}
   ngOnInit(): void {
     this.getIdAndFetchData();
   }
   getIdAndFetchData(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-
-    if (id !== null && id !== undefined) {
-      this.urlid = +id; // + işareti ile string'i number'a çeviriyoruz
+    this.route.queryParams.subscribe(params => {
+      this.urlid = +this.route.snapshot.paramMap.get('id')!;
+      this.musteriBilgi = {
+        siparisid: params['siparisid'],
+        name: params['name'],
+        mail: params['mail'],
+        tel: params['tel']
+      };
       this.fetchTeklifDetay(this.urlid);
-    } else {
-      // Hata durumunu veya default bir değeri ele alın
-      console.error('ID parametresi bulunamadı.');
-    }
+    });
   }
-  getir(){
-    const id = this.route.snapshot.paramMap.get('id');
-   
-    if (id !== null && id !== undefined) {
-      this.urlid = +id; // + işareti ile string'i number'a çeviriyoruz
-      this.item=this.getTeklifDetay(this.urlid as number);
-      return this.item;
-    } else {
-      // Hata durumunu veya default bir değeri ele alın
-      console.error('ID parametresi bulunamadı.');
-    }
-  }
+
   fetchTeklifDetay(id: number): void {
     this.teklifService.getByList(id.toString()).subscribe(
       data => {
@@ -59,10 +47,6 @@ export class TeklifdetayComponent {
   }
 
 
-
-  getTeklifDetay(id:number){
-    return this.teklifDetay.filter(p => p.teklifid === id);
-  }
   totalPrice() : number{
     let totalPrice = 0;
   
