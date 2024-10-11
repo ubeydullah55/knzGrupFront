c8a6839affae8677dd88998af55f97c7e11e425b
HISARHOSPITAL\ubeydullah.dogan <ubeydullah.dogan@hisarhospital.com>
2024-10-12 00:00:37 +0300

    liste alındı

--- src/app/pages/front/products/products.component.ts
+++ src/app/pages/front/products/products.component.ts
@@ -5,6 +5,8 @@ import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 import { DataService } from '../../../services/data-service.service';
 import { CategoryService } from '../../../services/category.service';
+import { productsModel } from '../../../models/productModel';
+import { categoryModel } from '../../../models/categoryModel';
 
 
 @Component({
@@ -22,13 +24,17 @@ export class ProductsComponent implements OnInit {
   productsService = inject(ProductsService);
   categoryService = inject(CategoryService);
   dataService = inject(DataService);
-  categories$=this.kategoriListele();
-  productsApi$=this.urunListele();
+  products: productsModel[] = [];
+  categories: categoryModel[] = [];
+
+
   constructor(private router: Router,@Inject(PLATFORM_ID) private platformId: Object) {}
   ngOnInit(): void {
-
+    this.kategoriListele();   // Ürünleri yükle
+    this.urunListele(); // Kategorileri yükle
 
   }
+
   // TrackBy function for *ngFor
   trackById(index: number, item: any): number {
     return item.id; // Her öğe için benzersiz olan ID'yi döndür
@@ -44,9 +50,24 @@ export class ProductsComponent implements OnInit {
     this.cartService.urunSil(id);
   }
   kategoriListele(){
-    return this.categoryService.getAllCategoryApi();
+    this.categoryService.getAllCategoryApi().subscribe({
+      next: (data) => {
+        this.categories = data; // Kategorileri al ve güncelle
+      },
+      error: (err) => {
+        console.error('Kategoriler alınırken bir hata oluştu:', err);
+      }
+    });
+
   }
   urunListele(){
-    return this.productsService.getAllProductsApi();
+    this.productsService.getAllProductsApi().subscribe({
+      next: (data) => {
+        this.products = data; // Ürünleri al ve güncelle
+      },
+      error: (err) => {
+        console.error('Ürünler alınırken bir hata oluştu:', err);
+      }
+    });
   }
 }
